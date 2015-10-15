eb5777ec3395d69cea31902676b12409d2d3febd
landonkail <landon.kail@gmail.com>
2015-10-15 11:32:09 -0400

    add file methods

---
+++ src/Forum.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+/**
+ * Created by landonkail on 10/15/15.
+ */
+public class Forum {
+    public static void main(String[] args) {
+
+    }
+
+    static String readFile(String fileName) {
+        File f = new File(fileName);
+        try {
+            FileReader fr = new FileReader(f);
+            int fileSize = (int) f.length();
+            char[] fileContent = new char[fileSize];
+            fr.read(fileContent);
+            return new String(fileContent);
+
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    static void writeFile(String fileName, String fileContent) {
+        File f = new File(fileName);
+        try {
+            FileWriter fw = new FileWriter(f);
+            fw.write(fileContent);
+            fw.close();
+        } catch (Exception e) {
+
+        }
+    }
+
+}
